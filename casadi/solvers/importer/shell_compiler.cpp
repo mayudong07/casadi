134760d5c1e4b71369486d1cbad6c52bd358d047
Joris Gillis; u0052373 <joris.gillis@kuleuven.be>
2017-08-27 15:33:46 +0200

    fixup! repair code for missing MKSTEMPS

--- casadi/solvers/importer/shell_compiler.cpp
+++ casadi/solvers/importer/shell_compiler.cpp
@@ -146,7 +146,7 @@ namespace casadi {
     if (mkstemps(obj_name, 2) == -1) {
       casadi_error("Failed to create a temporary object file name");
     }
-    std::string obj_name_ = obj_name;
+    obj_name_ = obj_name;
 
     char bin_name[] = "tmp_casadi_compiler_shell_XXXXXX.so";
     if (mkstemps(bin_name, 3) == -1) {
